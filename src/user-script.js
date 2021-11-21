48335b389b91556859c7f66600534dd12a9b380e
BaboonKing <changfan.xu.up@qq.com>
2021-11-22 00:22:49 +0800

    add userscript

---
+++ src/user-script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         qingshu-helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  qingshu-helper
+// @author       BaboonKing
+// @match        *://*.qingshuxuetang.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
